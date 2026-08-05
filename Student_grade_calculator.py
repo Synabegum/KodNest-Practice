c9c734fb035d7ccc12d23faec33c314dd8d73273
Syna <169091063+Synabegum@users.noreply.github.com>
2026-08-06 00:03:51 +0530

    Student_grade_calculator

--- Student_grade_calculator.py
+++ Student_grade_calculator.py
@@ -1,14 +1,16 @@
 marks = int(input())
 
-if marks < 0 or marks > 100:
-    print("Invalid Marks")
-elif marks >= 90:
+if marks >= 90 and marks <= 100:
     print("Grade: A")
-elif marks >= 75:
+elif marks >= 75 and marks <=89:
     print("Grade: B")
-elif marks >= 60:
+elif marks >= 60 and marks <= 74:
     print("Grade: C")
-elif marks >= 40:
+elif marks >= 40 and marks <= 59:
     print("Grade: D")
-else:
+elif marks >= 0 and marks <= 39:
     print("Grade: F")
+else:
+    print("Invalid Marks")
+
+
