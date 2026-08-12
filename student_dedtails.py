a219c0e78ec7a9f87819b43b41bdcfc2855af0be
Syna <169091063+Synabegum@users.noreply.github.com>
2026-08-12 13:46:12 +0530

    Pass student Details to a function

---
+++ student_dedtails.py
@@ -0,0 +1,8 @@
+def display_student(name, course):
+    print(f"Student: {name}")
+    print(f"Course: {course}")
+
+student_name = input()
+course_name = input()
+
+display_student(student_name, course_name)
